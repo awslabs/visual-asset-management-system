87bba93d60c77596084598e6df6742171da21c52
Archie Cowan <arccos@amazon.com>
2023-03-02 23:32:35 +0000

    fix(web): update create asset buttons

--- web/src/pages/Assets.js
+++ web/src/pages/Assets.js
@@ -4,20 +4,25 @@
  */
 
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { fetchAllAssets, fetchDatabaseAssets } from "../services/APIService";
 import CreateUpdateAsset from "../components/createupdate/CreateUpdateAsset";
 import { AssetListDefinition } from "../components/list/list-definitions/AssetListDefinition";
 import ListPage from "./ListPage";
 
 export default function Assets() {
+  const navigate = useNavigate();
+
   return (
     <ListPage
       singularName={"asset"}
       singularNameTitleCase={"Asset"}
       pluralName={"assets"}
       pluralNameTitleCase={"Assets"}
+      onCreateCallback={() => {
+        navigate("/upload");
+      }}
       listDefinition={AssetListDefinition}
-      CreateNewElement={CreateUpdateAsset}
       fetchAllElements={fetchAllAssets}
       fetchElements={fetchDatabaseAssets}
     />
