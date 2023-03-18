03497f20194963c8e1207a3761bf31695f370af8
Stephen Curtis <stephenlcurtis@gmail.com>
2023-03-17 21:46:17 -0700

    chore: code split app, workflow editor, plotter

--- web/src/index.js
+++ web/src/index.js
@@ -7,22 +7,24 @@
  * Establishes global css, fetches server-side resource,
  * configures amplify and initialized react app.
  */
-import React from "react";
+import React, {Suspense} from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.scss";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import ConfigLoader from "./ConfigLoader";
 
 window.LOG_LEVEL = "INFO";
 
+const App = React.lazy(() => import('./App'));
 ReactDOM.render(
-  <React.StrictMode>
-    <ConfigLoader>
-      <App />
-    </ConfigLoader>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <ConfigLoader>
+            <Suspense fallback={() => <></>}>
+                <App/>
+            </Suspense>
+        </ConfigLoader>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 reportWebVitals();
