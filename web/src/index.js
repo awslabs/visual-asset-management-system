a74a77cf442b84998498e3f8a2d87d780867fadd
Archie Cowan <arccos@amazon.com>
2023-03-29 14:13:59 +0000

    fix(web): Suspense fallback requires a component rather than a function.

--- web/src/index.js
+++ web/src/index.js
@@ -19,7 +19,7 @@ const App = React.lazy(() => import("./App"));
 ReactDOM.render(
     <React.StrictMode>
         <ConfigLoader>
-            <Suspense fallback={() => <></>}>
+            <Suspense fallback={<div />}>
                 <App />
             </Suspense>
         </ConfigLoader>
