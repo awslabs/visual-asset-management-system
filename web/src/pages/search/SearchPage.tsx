f976bada7601683cde0cc4816644c3b118d5d36f
Ravi Jadhav <jadravi@amazon.com>
2023-06-15 15:49:47 -0700

    search page simple

---
+++ web/src/pages/search/SearchPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface SearchPageProps {
+
+}
+
+function SearchPage(props: SearchPageProps) {
+    return (
+        <div>
+            <h1> Hello world!
+            </h1>
+        </div>
+    );
+}
+
+export default SearchPage;
