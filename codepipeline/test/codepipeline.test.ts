a61e75934d1b1b77f8a6db45af50a5fd00cb2cef
Ami <apateamz@amazon.com>
2023-11-18 22:00:58 -0500

    add empty test for codepipeline

--- codepipeline/test/codepipeline.test.ts
+++ codepipeline/test/codepipeline.test.ts
@@ -1,17 +1,6 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Codepipeline from '../lib/codepipeline-stack';
-
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/codepipeline-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Codepipeline.CodepipelineStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
-
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+/*
+ * Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+test("Empty Stack", () => {
 });
