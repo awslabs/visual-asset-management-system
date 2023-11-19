97c3a135506b9d400a917d1477d08af12a449737
Ami <apateamz@amazon.com>
2023-11-19 09:31:29 -0500

    run prettier

--- codepipeline/bin/codepipeline.ts
+++ codepipeline/bin/codepipeline.ts
@@ -5,11 +5,11 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
 import { AwsSolutionsChecks } from "cdk-nag";
 import { Aspects } from "aws-cdk-lib";
-import {CodePipelineStack} from "../lib/codepipeline";
+import { CodePipelineStack } from "../lib/codepipeline";
 
 const app = new cdk.App();
 const region = process.env.AWS_REGION || "us-east-1";
@@ -21,9 +21,9 @@ if (enableCdkNag) {
     Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }));
 }
 
-new CodePipelineStack(app,  `vams-code-pipeline-${process.env.DEPLOYMENT_ENV || "dev"}`, {
+new CodePipelineStack(app, `vams-code-pipeline-${process.env.DEPLOYMENT_ENV || "dev"}`, {
     stackName: `vams-code-pipeline-${process.env.DEPLOYMENT_ENV || "dev"}`,
-    env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: region }
+    env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: region },
 });
 
 app.synth();
--- codepipeline/test/codepipeline.test.ts
+++ codepipeline/test/codepipeline.test.ts
@@ -2,5 +2,4 @@
  * Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
-test("Empty Stack", () => {
-});
+test("Empty Stack", () => {});
