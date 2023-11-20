a6cdca2c6479fd3f3c81a6f4dff4a4f85551054e
Ami <apateamz@amazon.com>
2023-11-20 12:45:57 -0500

    undo changes to infra tests

--- infra/test/infra.test.ts
+++ infra/test/infra.test.ts
@@ -10,22 +10,14 @@ import * as Infra from "../lib/infra-stack";
 test("Empty Stack", () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new Infra.VAMS(app, "MyTestStack", {
-        env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: "us-east-1" },
-        prod: false,
-        stackName: `vams--test`,
-        ssmWafArnParameterName: "",
-        ssmWafArnParameterRegion: "",
-        ssmWafArn: "",
-        stagingBucket: "",
-    });
+    const stack = new Infra.VAMS(app, "MyTestStack", { prod: false, stackName: `vams--test` });
     // THEN
     expectCDK(stack).to(
         matchTemplate(
             {
                 Resources: {},
             },
-            MatchStyle.EXACT,
-        ),
+            MatchStyle.EXACT
+        )
     );
 });
