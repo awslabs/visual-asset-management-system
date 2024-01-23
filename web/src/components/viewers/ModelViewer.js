6be51c313637819bf689892b4efef7e0b62fc492
petedk <petedk@amazon.co.uk>
2024-01-23 15:13:08 +0000

    Added a brighter environment map as default

--- web/src/components/viewers/ModelViewer.js
+++ web/src/components/viewers/ModelViewer.js
@@ -7,6 +7,15 @@ import React, { useEffect, useRef, useState } from "react";
 import * as OV from "online-3d-viewer";
 import { getPresignedKey } from "../../common/auth/s3";
 
+const cubeMap = [
+    process.env.PUBLIC_URL + `/cubeMap/posx.jpg`,
+    process.env.PUBLIC_URL + `/cubeMap/negx.jpg`,
+    process.env.PUBLIC_URL + `/cubeMap/posy.jpg`,
+    process.env.PUBLIC_URL + `/cubeMap/negy.jpg`,
+    process.env.PUBLIC_URL + `/cubeMap/posz.jpg`,
+    process.env.PUBLIC_URL + `/cubeMap/negz.jpg`
+];
+
 export default function ModelViewer(props) {
     const engineElement = useRef(null);
     const { assetId, databaseId, assetKey } = props;
@@ -18,7 +27,9 @@ export default function ModelViewer(props) {
                 engineElement.current.setAttribute("model", remoteFileUrl);
                 setTimeout(() => {
                     let parentDiv = engineElement.current;
+                    let environmentSettings = new OV.EnvironmentSettings(cubeMap);
                     let viewer = new OV.EmbeddedViewer(parentDiv, {
+                        environmentSettings: environmentSettings,
                         backgroundColor: new OV.RGBAColor(182, 182, 182, 182),
                         defaultColor: new OV.RGBColor(200, 200, 200),
                         edgeSettings: new OV.EdgeSettings(true, new OV.RGBColor(0, 0, 255), 1),
