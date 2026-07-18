77762a022ef49f6e7d1b8826a84041df291e4be3
Kurt Scheuringer <scheurik@amazon.com>
2026-07-18 19:12:00 +0200

    test(web): use non-deprecated act import for React 18

--- web/src/components/interactive/WorkflowEditor.test.tsx
+++ web/src/components/interactive/WorkflowEditor.test.tsx
@@ -3,8 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { render } from "@testing-library/react";
-import { act } from "react-dom/test-utils";
+import { render, act } from "@testing-library/react";
 import createWrapper from "@cloudscape-design/components/test-utils/dom";
 import WorkflowEditor, { workflowPipelineToElements } from "./WorkflowEditor";
 import { WorkflowContext } from "../../context/WorkflowContext";
